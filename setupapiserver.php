954b8496ea3f92839cc13d42dc5d0f265c9b1c5e
arnut <arnut@rvglobalsoft.com>
2019-10-22 13:36:14 +0700

    debug

--- setupapiserver.php
+++ setupapiserver.php
@@ -1,7 +1,5 @@
 <?php
 
-ob_start();
-
 register_shutdown_function(function() {
     $error = error_get_last();
     if(null !== $error) {
@@ -9,8 +7,10 @@ register_shutdown_function(function() {
         $response['status'] = false;
         $response['exectime'] = '';
         $response['message'] = 'This error has to be fixed by RVsitebuilder team. Please submit a ticket with Hosting Access and Domain name provided to us directly <a href="https://rvglobalsoft.com/tickets/new&deptId=5" target="_blank">here.</a>';
-        header('Content-type: application/json');
-        echo json_encode( $data );
+        if (!headers_sent()) {
+            header('Content-type: application/json');
+        }
+        echo json_encode( $response);
         exit;
     }
 });
