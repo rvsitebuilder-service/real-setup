2fa19a1c694c2445b8199bcb1e550081c2ca7d20
Nattawut Nanopsuntornsil <nattawut@netway.co.th>
2021-03-02 11:37:37 +0700

    rename variable

--- RVsitebuilder_Setup_API.php
+++ RVsitebuilder_Setup_API.php
@@ -544,10 +544,16 @@ class RVsitebuilder_Setup_API
         }
         $this->print_debug_log("Copy $source To $destination");
 
-        //delete cacheAppSetting.json
-        if ($files->exists($homeuser . '/rvsitebuildercms/' . $domainname . '/storage/cacheAppSetting.json')) {
-            $files->remove($homeuser . '/rvsitebuildercms/' . $domainname . '/storage/cacheAppSetting.json');
-            $this->print_debug_log("Removed " . $homeuser . '/rvsitebuildercms/' . $domainname . '/storage/cacheAppSetting.json');
+
+        $pathConfig = $homeuser . '/rvsitebuildercms/' . $domainname . '/storage/appsconfig.json';
+        if ($files->exists($pathConfig)) {
+            $files->remove($pathConfig);
+            $this->print_debug_log("Removed " . $pathConfig);
+        }
+        $pathSetting = $homeuser . '/rvsitebuildercms/' . $domainname . '/storage/cacheAppSetting.json';
+        if ($files->exists($pathSetting)) {
+            $files->remove($pathSetting);
+            $this->print_debug_log("Removed " . $pathSetting);
         }
 
         //delete framwork package
@@ -618,11 +624,16 @@ class RVsitebuilder_Setup_API
             return $res;
         }
 
-        //delete cacheAppSetting.json
         $files = new Filesystem();
-        if (file_exists(dirname(__FILE__) . '/tmp/storage/cacheAppSetting.json')) {
-            $files->remove(dirname(__FILE__) . '/tmp/storage/cacheAppSetting.json');
-            $this->print_debug_log("Removed " . dirname(__FILE__) . '/tmp/storage/cacheAppSetting.json');
+        $pathConfig = dirname(__FILE__) . '/tmp/storage/appsconfig.json';
+        if (file_exists($pathConfig)) {
+            $files->remove($pathConfig);
+            $this->print_debug_log("Removed " . $pathConfig);
+        }
+        $pathSetting = dirname(__FILE__) . '/tmp/storage/cacheAppSetting.json';
+        if (file_exists($pathSetting)) {
+            $files->remove($pathSetting);
+            $this->print_debug_log("Removed " . $pathSetting);
         }
 
         //delete framwork package
